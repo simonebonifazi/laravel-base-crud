e78cb0e07f0aa61408d18c049fc8a21e19b04f0a
simonebonifazi <simonbonifraz@gmail.com>
2022-09-27 00:15:23 +0200

    added validation with bootstrap

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -21,7 +21,7 @@
     <!-- titolo -->
     <div class="mb-3">
         <label for="title" class="form-label">Titolo fumetto...</label>
-        <input class="form-control" type="text" id="title" name="title" placeholder="es. capitan america"
+        <input class="form-control  @error('title') is-invalid @enderror" type="text" id="title" name="title" placeholder="es. capitan america"
         value=" {{ old('title' }} ">
     </div>
     <!-- descizione -->
@@ -33,31 +33,31 @@
     <!-- thumb -->
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine di copertina </label>
-        <input class="form-control" type="text" id="thumb" name="thumb" placeholder="Url della cover..."
+        <input class="form-control @error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb" placeholder="Url della cover..."
         value=" {{ old('thumb' }}">
     </div>
     <!-- price -->
     <div class="mb-3">
         <label for="price" class="form-label">Il tuo prezzo</label>
-        <input class="form-control" type="text" id="price" name="price" placeholder="scegli un prezzo ragionevole"
+        <input class="form-control @error('price') is-invalid @enderror" type="text" id="price" name="price" placeholder="scegli un prezzo ragionevole"
         value=" {{ old('price' }}">
     </div>
     <!-- sale_date // cambiare prospettiva rispetto da utente finale a chi riceve progetto in mano-->
     <div class="mb-3">
         <label for="sale_date" class="form-label">Scadenza offerta</label>
-        <input class="form-control" type="text" id="sale_date" name="sale_date" placeholder="es.: 2023-08-22"
+        <input class="form-control @error('sale_date') is-invalid @enderror" type="text" id="sale_date" name="sale_date" placeholder="es.: 2023-08-22"
         value=" {{ old('sale_date' }}">
     </div>
     <!-- series -->
     <div class="mb-3">
         <label for="series" class="form-label">La Serie del tuo fumetto</label>
-        <input class="form-control" type="text" id="series" name="series" placeholder="es. aquaman, batman..."
+        <input class="form-control @error('series') is-invalid @enderror" type="text" id="series" name="series" placeholder="es. aquaman, batman..."
         value=" {{ old('series' }}">
     </div>
     <!-- type -->
     <div class="mb-3">
         <label for="type" class="form-label">Che tipo di fumetto vuoi vendere?</label>
-        <input class="form-control" type="text" id="type" name="type" placeholder="es. graphic novel, comic book..."
+        <input class="form-control @error('type') is-invalid @enderror" type="text" id="type" name="type" placeholder="es. graphic novel, comic book..."
         value=" {{ old('type'}}">
         <!-- # il value old('nome attributo') fa si che se c'è un errore, verrà reimpostato ciò che è stato scritto dell'utente in precedenza, al ricaricare della pagina dopo l'invio dei dati
             # ne consegue che nel form congiunto edit e create dovrò aggiungere secondo parametro
--- resources/views/Comics/edit.blade.php
+++ resources/views/Comics/edit.blade.php
@@ -10,7 +10,7 @@
     <!-- titolo -->
     <div class="mb-3">
         <label for="title" class="form-label">Titolo fumetto...</label>
-        <input class="form-control" type="text" id="title" name="title" placeholder="es. capitan america"
+        <input class="form-control @error('title') is-invalid @enderror" type="text" id="title" name="title" placeholder="es. capitan america"
             value="{{ old('title', $comic->title) }}">
     </div>
     <!-- descizione -->
@@ -21,31 +21,31 @@
     <!-- thumb -->
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine di copertina </label>
-        <input class="form-control" type="text" id="thumb" name="thumb" placeholder="Url della cover..."
+        <input class="form-control @error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb" placeholder="Url della cover..."
             value="{{ old('thumb' , '$comic->thumb') }}">
     </div>
     <!-- price -->
     <div class="mb-3">
         <label for="price" class="form-label">Il tuo prezzo</label>
-        <input class="form-control" type="text" id="price" name="price" placeholder="scegli un prezzo ragionevole..."
+        <input class="form-control @error('price') is-invalid @enderror" type="text" id="price" name="price" placeholder="scegli un prezzo ragionevole..."
             value="{{ old('price','$comic->price') }}">
     </div>
     <!-- sale_date // "parafrasato"-->
     <div class="mb-3">
         <label for="sale_date" class="form-label">Scadenza della tua offerta</label>
-        <input class="form-control" type="text" id="sale_date" name="sale_date" placeholder="es.: 2016-08-22"
+        <input class="form-control @error('sale_date') is-invalid @enderror" type="text" id="sale_date" name="sale_date" placeholder="es.: 2016-08-22"
             value="{{ old('sale_date','$comic->sale_date') }}">
     </div>
     <!-- series -->
     <div class="mb-3">
         <label for="series" class="form-label">La Serie del tuo fumetto</label>
-        <input class="form-control" type="text" id="series" name="series" placeholder="es. aquaman, batman..."
+        <input class="form-control @error('series') is-invalid @enderror" type="text" id="series" name="series" placeholder="es. aquaman, batman..."
             value="{{ old('series','$comic->series') }}">
     </div>
     <!-- type -->
     <div class="mb-3">
         <label for="type" class="form-label">Che tipo di fumetto vuoi vendere?</label>
-        <input class="form-control" type="text" id="type" name="type" placeholder="es. graphic novel, comic book..."
+        <input class="form-control @error('type') is-invalid @enderror " type="text" id="type" name="type" placeholder="es. graphic novel, comic book..."
             value="{{ old('type','$comic->type') }}">
     </div>
     <!-- buttons & a -->
