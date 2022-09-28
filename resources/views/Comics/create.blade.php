73e5fac8f774141b0781da863c3779f72aca848a
simonebonifazi <simonbonifraz@gmail.com>
2022-09-28 08:33:45 +0200

    create form fixed

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -21,44 +21,43 @@
     <!-- titolo -->
     <div class="mb-3">
         <label for="title" class="form-label">Titolo fumetto...</label>
-        <input class="form-control  @error('title') is-invalid @enderror" type="text" id="title" name="title" placeholder="es. capitan america"
-        value=" {{ old('title' }} ">
+        <input class="form-control  @error('title') is-invalid @enderror" type="text" id="title" name="title"
+            placeholder="es. capitan america" value=" {{ old('title') }} ">
     </div>
     <!-- descizione -->
     <div class="mb-3">
         <label for="description" class="form-label">Descrizione: </label>
-        <textarea class="form-control" id="description" name="description"
-        value=" {{ old('description' }}" > </textarea>
+        <textarea class="form-control" id="description" name="description"> {{ old('description') }}</textarea>
     </div>
     <!-- thumb -->
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine di copertina </label>
-        <input class="form-control @error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb" placeholder="Url della cover..."
-        value=" {{ old('thumb' }}">
+        <input class="form-control @error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb"
+            placeholder="Url della cover..." value=" {{ old('thumb') }}">
     </div>
     <!-- price -->
     <div class="mb-3">
         <label for="price" class="form-label">Il tuo prezzo</label>
-        <input class="form-control @error('price') is-invalid @enderror" type="text" id="price" name="price" placeholder="scegli un prezzo ragionevole"
-        value=" {{ old('price' }}">
+        <input class="form-control @error('price') is-invalid @enderror" type="text" id="price" name="price"
+            placeholder="scegli un prezzo ragionevole" value=" {{ old('price') }}">
     </div>
     <!-- sale_date // cambiare prospettiva rispetto da utente finale a chi riceve progetto in mano-->
     <div class="mb-3">
         <label for="sale_date" class="form-label">Scadenza offerta</label>
-        <input class="form-control @error('sale_date') is-invalid @enderror" type="text" id="sale_date" name="sale_date" placeholder="es.: 2023-08-22"
-        value=" {{ old('sale_date' }}">
+        <input class="form-control @error('sale_date') is-invalid @enderror" type="text" id="sale_date" name="sale_date"
+            placeholder="es.: 2023-08-22" value=" {{ old('sale_date') }}">
     </div>
     <!-- series -->
     <div class="mb-3">
         <label for="series" class="form-label">La Serie del tuo fumetto</label>
-        <input class="form-control @error('series') is-invalid @enderror" type="text" id="series" name="series" placeholder="es. aquaman, batman..."
-        value=" {{ old('series' }}">
+        <input class="form-control @error('series') is-invalid @enderror" type="text" id="series" name="series"
+            placeholder="es. aquaman, batman..." value=" {{ old('series') }}">
     </div>
     <!-- type -->
     <div class="mb-3">
         <label for="type" class="form-label">Che tipo di fumetto vuoi vendere?</label>
-        <input class="form-control @error('type') is-invalid @enderror" type="text" id="type" name="type" placeholder="es. graphic novel, comic book..."
-        value=" {{ old('type'}}">
+        <input class="form-control @error('type') is-invalid @enderror" type="text" id="type" name="type"
+            placeholder="es. graphic novel, comic book..." value=" {{ old('type')}}">
         <!-- # il value old('nome attributo') fa si che se c'è un errore, verrà reimpostato ciò che è stato scritto dell'utente in precedenza, al ricaricare della pagina dopo l'invio dei dati
             # ne consegue che nel form congiunto edit e create dovrò aggiungere secondo parametro
             : old('type', $comic->type) , che cerca il valore per il campo nel db   -->
